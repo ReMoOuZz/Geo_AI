8d3c946e17a80a45103d80df6a7a340fc2912140
remi <203637481+ReMoOuZz@users.noreply.github.com>
2025-08-18 17:38:52 +0200

    added actions to Chats controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,9 +1,20 @@
 class ChatsController < ApplicationController
+  before_action :set_params
 
   def create
+    @chat = Chat.new(chat_params)
   end
 
   def show
   end
 
+  private
+
+  def chat_params
+    params.require(:chat).permit(:title, :user_id, :quiz_id)
+  end
+
+  def set_params
+    @quiz = Quiz.find(params[:id])
+  end
 end
