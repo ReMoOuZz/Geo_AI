e07a6320803763ca76f3d2d4276489285b9d2fdb
Dylane Piro <piro.dylan@outlook.fr>
2025-08-19 16:04:19 +0200

    fixed model in app

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :chats, only: [:create, :show] do
-    resources :answers, only: [:create]
+    resources :messages, only: [:create]
   end
   resources :quizzes, only: [:show]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
