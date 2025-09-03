c5800e19d2a2172ced6b09c6a49f0c26a2168b42
remi <203637481+ReMoOuZz@users.noreply.github.com>
2025-09-03 14:06:17 +0200

    commit for merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,6 +15,7 @@ class PagesController < ApplicationController
     @incorrect = @answers.joins(:game_question).where(is_correct: false || nil)
 
     correct = @correct.count
+    incorrect = @incorrect.count
     total = @total_questions.count
 
     @total_correct = total.positive? ? (correct.fdiv(total) * 100).round(1) : 0.0
@@ -22,11 +23,31 @@ class PagesController < ApplicationController
     cat = [
       "Hydrographie et reliefs",
       "Régions et villes du monde",
-      "Cultures", "Géopolitique",
+      "Cultures",
+      "Géopolitique",
       "Drapeaux"
     ]
     @correct_in_cat = @correct.where(game_questions: { category: cat })
 
+    @correct_in_cat_hr = @correct.where(game_questions: { category: "Hydrographie et reliefs" })
+    @incorrect_in_cat_hr = @incorrect.where(game_questions: { category: "Hydrographie et reliefs" })
+    @total_correct_hr = incorrect.positive? ? (@correct_in_cat_hr.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_cat_rv = @correct.where(game_questions: { category: "Régions et villes du monde" })
+    @incorrect_in_cat_rv = @incorrect.where(game_questions: { category: "Régions et villes du monde" })
+    @total_correct_rv = incorrect.positive? ? (@correct_in_cat_rv.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_cat_cl = @correct.where(game_questions: { category: "Cultures" })
+    @incorrect_in_cat_cl = @incorrect.where(game_questions: { category: "Cultures" })
+    @total_correct_cl = incorrect.positive? ? (@correct_in_cat_cl.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_cat_gp = @correct.where(game_questions: { category: "Géopolitique" })
+    @incorrect_in_cat_gp = @incorrect.where(game_questions: { category: "Géopolitique" })
+    @total_correct_gp = incorrect.positive? ? (@correct_in_cat_gp.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_cat_dp = @correct.where(game_questions: { category: "Drapeaux" })
+    @incorrect_in_cat_dp = @incorrect.where(game_questions: { category: "Drapeaux" })
+    @total_correct_dp = incorrect.positive? ? (@correct_in_cat_dp.count.fdiv(total) * 100).round(1) : 0.0
 
     dif = [
       "facile",
