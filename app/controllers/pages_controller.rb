e9ab0e8bd9e904adf804b6a9f64984f54f18badb
remi <203637481+ReMoOuZz@users.noreply.github.com>
2025-09-03 14:41:24 +0200

    completed logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -56,6 +56,18 @@ class PagesController < ApplicationController
     ]
     @correct_in_dif = @correct.where(game_questions: { difficulty: dif })
 
+    @correct_in_dif_f = @correct.where(game_questions: { difficulty: "facile" })
+    @incorrect_in_dif_f = @incorrect.where(game_questions: { difficulty: "facile" })
+    @total_correct_dif_f = incorrect.positive? ? (@correct_in_dif_f.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_dif_i = @correct.where(game_questions: { difficulty: "intermédiaire" })
+    @incorrect_in_dif_i = @incorrect.where(game_questions: { difficulty: "intermédiaire" })
+    @total_correct_dif_i = incorrect.positive? ? (@correct_in_dif_i.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_dif_d = @correct.where(game_questions: { difficulty: "difficile" })
+    @incorrect_in_dif_d = @incorrect.where(game_questions: { difficulty: "difficile" })
+    @total_correct_dif_d = incorrect.positive? ? (@correct_in_dif_d.count.fdiv(total) * 100).round(1) : 0.0
+
 
     reg = [
       "Europe",
@@ -66,11 +78,28 @@ class PagesController < ApplicationController
     ]
     @correct_in_reg = @correct.where(game_questions: { region: reg })
 
+    @correct_in_reg_eu = @correct.where(game_questions: { region: "Europe" })
+    @incorrect_in_reg_eu = @incorrect.where(game_questions: { region: "Europe" })
+    @total_correct_reg_eu = incorrect.positive? ? (@correct_in_reg_eu.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_reg_am = @correct.where(game_questions: { region: "Amérique" })
+    @incorrect_in_reg_am = @incorrect.where(game_questions: { region: "Amérique" })
+    @total_correct_reg_am = incorrect.positive? ? (@correct_in_reg_am.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_reg_oc = @correct.where(game_questions: { region: "Océanie" })
+    @incorrect_in_reg_oc = @incorrect.where(game_questions: { region: "Océanie" })
+    @total_correct_reg_oc = incorrect.positive? ? (@correct_in_reg_oc.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_reg_af = @correct.where(game_questions: { region: "Afrique" })
+    @incorrect_in_reg_af = @incorrect.where(game_questions: { region: "Afrique" })
+    @total_correct_reg_af = incorrect.positive? ? (@correct_in_reg_af.count.fdiv(total) * 100).round(1) : 0.0
+
+    @correct_in_reg_as = @correct.where(game_questions: { region: "Asie" })
+    @incorrect_in_reg_as = @incorrect.where(game_questions: { region: "Asie" })
+    @total_correct_reg_as = incorrect.positive? ? (@correct_in_reg_as.count.fdiv(total) * 100).round(1) : 0.0
 
     @user_games = current_user.games.includes(:scores)
     @user_allscores = @user_games.map{|g| g.scores}.flatten
     @nb_total_game = @user_games.count
-
   end
-
 end
